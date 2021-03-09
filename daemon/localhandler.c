shawnanastasio/libkvmchan

commit 5e81fa85cb12e959b5acc38ed4ca137db1815c03

daemon: Fix race between localhandler and ivshmem client requests
In the complicated web of subprocesses and IPC that is kvmchand,
client requests that propagate throughout the entire daemon can be
initiated from two places:
    * localhandler.c for clients on the local domain using the unix
      socket API.
and
    * (host only) ivshmem.c for clients on guest domains that have
      kvmchand-guest instances running and communicate.

Both of these request entrypoints end up calling the same internal
IPC endpoints, none of which were designed with reentrancy in mind.
This creates an obvious issue in the case where a request originates
from one entrypoint and before it is finished, another request originates
from the other entrypoint. This has the potential to cause Bad Things to
happen (namely, daemon-wide deadlocks).

This issue was not discovered during initial development and testing which
used simple, manually-initiated test programs. When trying with a real Qubes
environment though, the vast number of vchans and daemons that all start up
when a VM is launch resulted in the race being hit quite frequently.

There would be two routes to fix this issue: Modifying all IPC endpoints
to support reentrancy or adding a global request lock that ensures only
one entrypoint is servicing a request at a time. As the former would require
a massive overhaul of the daemon, this commit implements the latter.

Locking is implemented with an eventfd in EFD_SEMAPHORE mode that is created
by localhandler and passed to ivshmem over IPC during early init. Both client
request event loops were then modified to acquire a lock on the eventfd before
servicing a given request and to drop it afterwards. This change was minimally
invasive and seems to have completely resolved all deadlocks during testing.

daemon/localhandler.c
@@ -598,6 +598,29 @@ void run_localhandler_loop(int mainsoc, bool is_dom0) {
     }
 #endif /* HAVE_SYSTEMD */
 
+    // Now that we're initialized, create a semaphore eventfd and send it to ivshmem
+    // so that it can begin handling guest requests as well.
+    int request_sem_eventfd = eventfd_sem_init(1);
+    if (request_sem_eventfd < 0)
+        goto error;
+
+    struct ipc_message ipc_msg = {
+        .type = IPC_TYPE_CMD,
+        .cmd = {
+            .command = IVSHMEM_IPC_CMD_START_LISTENING,
+        },
+        .fd_count = 1,
+        .fds = {request_sem_eventfd},
+        .dest = IPC_DEST_IVSHMEM,
+        .flags = IPC_FLAG_WANTRESP | IPC_FLAG_FD,
+    };
+
+    struct ipc_message ipc_resp;
+    if (!ipc_send_message(&ipc_msg, &ipc_resp))
+        goto error;
+    if (ipc_resp.resp.error)
+        goto error;
+
     // Poll for events
     struct epoll_event events[5];
     int event_count;
@@ -650,8 +673,12 @@ void run_localhandler_loop(int mainsoc, bool is_dom0) {
                 if (!client)
                     log_BUG("Failed to find client!");
 
-                // Valid message received, handle it
-                if (!handle_client_message(client, &msg))
+                // Valid message received, obtain client request semaphore and handle it
+                eventfd_sem_wait(request_sem_eventfd);
+                bool res = handle_client_message(client, &msg);
+                eventfd_sem_post(request_sem_eventfd);
+
+                if (!res)
                     if (!remove_connection(&data, fd))
                         log(LOGL_WARN, "Failed to delete client.");
            }

daemon/ipc.h
@@ -209,11 +209,11 @@ struct ipc_message {
  * args[1] - (u32) IVPosition of connection
  *
  * resp.error - error?
- * fds[0] - memfd
- * fds[1] - incoming eventfd 0
- * fds[2] - incoming eventfd 1
- * fds[3] - outgoing eventfd 0
- * fds[4] - outgoing eventfd 1
+ * resp.fds[0] - memfd
+ * resp.fds[1] - incoming eventfd 0
+ * resp.fds[2] - incoming eventfd 1
+ * resp.fds[3] - outgoing eventfd 0
+ * resp.fds[4] - outgoing eventfd 1
  */
 #define IVSHMEM_IPC_CMD_GET_CONN_FDS 1
 
@@ -227,6 +227,14 @@ struct ipc_message {
  * resp.error - error?
  */
 #define IVSHMEM_IPC_CMD_UNREGISTER_CONN 2
+
+/**
+ * Start listening for kvmchand requests from guests.
+ * fds[0] - Semaphore eventfd (EFD_SEMAPHORE) that must be held while processing requests.
+ *          Shared with localhandler to ensure that only one is processing client requests at
+ *          a given time.
+ */
+#define IVSHMEM_IPC_CMD_START_LISTENING 3
 
 // VFIO process commands
 

daemon/ivshmem.c
@@ -92,7 +92,14 @@ struct ivshmem_server {
     struct vec_voidp clients; // client_info vec
 };
 
-struct ivshmem_server g_server;
+static struct {
+    bool initialized;
+    pthread_cond_t cond;
+    pthread_mutex_t mutex;
+} g_init_status;
+
+static struct ivshmem_server g_server;
+static int g_request_sem_eventfd = -1;
 
 /**
  * Send an ivshmem protocol message to a given socket.
@@ -488,7 +495,13 @@ static void *conn_listener_thread(void *client_) {
             if (cur_fd == client->listener_eventfd)
                 goto fail_epoll_create;
 
-            if (!handle_kvmchand_message(client, conn)) {
+            // Obtain client request semaphore and handle message
+            if (!eventfd_sem_wait_or(g_request_sem_eventfd, client->listener_eventfd))
+                goto fail_epoll_create; // listener kill eventfd notified - time to exit
+            bool res = handle_kvmchand_message(client, conn);
+            eventfd_sem_post(g_request_sem_eventfd);
+
+            if (!res) {
                 log(LOGL_ERROR, "Couldn't handle message from client! Continuing...");
                 continue;
             }
@@ -830,6 +843,26 @@ static void handle_ipc_message(struct ipc_message *msg) {
             break;
         }
 
+        case IVSHMEM_IPC_CMD_START_LISTENING:
+        {
+            if (msg->fd_count != 1) {
+                log(LOGL_ERROR, "Received invalid CMD_START_LISTENING: wrong # of fds provided\n");
+                response.resp.error = true;
+                break;
+            }
+
+            // Save eventfd and signal to main thread that it can start listening
+            g_request_sem_eventfd = msg->fds[0];
+
+            ASSERT(!pthread_mutex_lock(&g_init_status.mutex));
+            g_init_status.initialized = true;
+            ASSERT(!pthread_cond_signal(&g_init_status.cond));
+            ASSERT(!pthread_mutex_unlock(&g_init_status.mutex));
+
+            response.resp.error = false;
+            break;
+        }
+
         default:
             log_BUG("Unknown IPC command received in ivshmem loop: %"PRIu64, cmd->command);
     }
@@ -841,6 +874,10 @@ static void handle_ipc_message(struct ipc_message *msg) {
 }
 
 void run_ivshmem_loop(int mainsoc) {
+    g_init_status.initialized = false;
+    ASSERT(!pthread_cond_init(&g_init_status.cond, NULL));
+    ASSERT(!pthread_mutex_init(&g_init_status.mutex, NULL));
+
     if (!ipc_start(mainsoc, IPC_DEST_IVSHMEM, handle_ipc_message))
         goto error;
 
@@ -877,6 +914,19 @@ void run_ivshmem_loop(int mainsoc) {
     g_server.socfd = socfd;
     if (!vec_voidp_init(&g_server.clients, 10, client_info_destructor))
         goto error;
+
+    // Wait for CMD_START_LISTENING before entering the event loop
+    if (!g_init_status.initialized) {
+        ASSERT(!pthread_mutex_lock(&g_init_status.mutex));
+        if (g_init_status.initialized)
+            goto already_initialized;
+
+        ASSERT(!pthread_cond_wait(&g_init_status.cond, &g_init_status.mutex));
+        ASSERT(g_init_status.initialized);
+
+    already_initialized:
+        ASSERT(!pthread_mutex_unlock(&g_init_status.mutex));
+    }
 
     // Poll for events
     struct epoll_event events[5];

daemon/util.c
@@ -28,6 +28,7 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/epoll.h>
+#include <sys/eventfd.h>
 
 #include "util.h"
 
@@ -263,5 +264,59 @@ ssize_t socmsg_recv(int socfd, void *buf, size_t len, int *fd_out) {
 
 out:
     return s;
+}
+
+int eventfd_sem_init(uint64_t initial_value) {
+    return eventfd(initial_value, EFD_SEMAPHORE | EFD_NONBLOCK);
+}
+
+void eventfd_sem_post(int evfd) {
+    uint64_t buf = 1;
+    ASSERT(write(evfd, &buf, sizeof(buf)) >= 0);
+}
+
+void eventfd_sem_wait(int evfd) {
+    uint64_t buf;
+    fd_set rfds;
+    for (;;) {
+        FD_ZERO(&rfds);
+        FD_SET(evfd, &rfds);
+        ASSERT(select(evfd + 1, &rfds, NULL, NULL, NULL) >= 0);
+
+        if (FD_ISSET(evfd, &rfds)) {
+            // The eventfd was ready for read, though it is possible
+            // that in the time since select(2)'s return someone else acquired
+            // the lock, so we have to be prepared to loop again in case of failure.
+            if (read(evfd, &buf, sizeof(buf)) >= 0)
+                break; // Success
+        }
+    }
+}
+
+/**
+ * Wait for a semaphore eventfd to become readable OR for another fd to become readable.
+ *
+ * @return whether the eventfd was the one that became readable
+ */
+bool eventfd_sem_wait_or(int evfd, int other) {
+    uint64_t buf;
+    fd_set rfds;
+    for (;;) {
+        FD_ZERO(&rfds);
+        FD_SET(evfd, &rfds);
+        FD_SET(other, &rfds);
+        ASSERT(select(MAX(evfd, other) + 1, &rfds, NULL, NULL, NULL) >= 0);
+
+        if (FD_ISSET(evfd, &rfds)) {
+            // The eventfd was ready for read, though it is possible
+            // that in the time since select(2)'s return someone else acquired
+            // the lock, so we have to be prepared to loop again in case of failure.
+            if (read(evfd, &buf, sizeof(buf)) >= 0)
+                return true;
+        } else if (FD_ISSET(other, &rfds)) {
+            // `other` is ready for read - return early
+            return false;
+        }
+    }
 }
 

daemon/util.h
@@ -30,6 +30,8 @@
 
 #define ROUND_UP(N, S) ((((N) + (S) - 1) / (S)) * (S))
 
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+
 // Old glibc doesn't have <sys/memfd.h>, just declare memfd_create manually
 #if __has_include(<sys/memfd.h>)
 #include <sys/memfd.h>
@@ -107,6 +109,11 @@ bool str_is_number(const char *str);
 
 int add_epoll_fd(int epoll_fd, int fd, int event);
 int del_epoll_fd(int epoll_fd, int fd);
+
+int eventfd_sem_init(uint64_t initial_value);
+void eventfd_sem_post(int evfd);
+void eventfd_sem_wait(int evfd);
+bool eventfd_sem_wait_or(int evfd, int other);
 
 bool install_exit_callback(void (*func)(void*), void *arg);
 void run_exit_callbacks(void);

commit 27158660d295a8857dc5e799c3eec8d15f7eeb9c

daemon: Fix localhandler crash when in guest mode
Commit 5e81fa85cb12 ("daemon: Fix race between localhandler and
ivshmem client requests") introduced a bug where localhandler.c would
cause an IPC crash when run in guest mode. This is due to code it
added that would unconditionally send a message to ivshmem.c to set
up the client request lock; this works fine on dom0 but in guest mode
there is no ivshmem.c process running so this results in an IPC bug
assertion.

Fix the issue by guarding the client request lock setup by a check for
`is_dom0` so that the wrong code paths aren't executed in guest mode.

daemon/localhandler.c
@@ -599,27 +599,30 @@ void run_localhandler_loop(int mainsoc, bool is_dom0) {
 #endif /* HAVE_SYSTEMD */
 
     // Now that we're initialized, create a semaphore eventfd and send it to ivshmem
-    // so that it can begin handling guest requests as well.
-    int request_sem_eventfd = eventfd_sem_init(1);
-    if (request_sem_eventfd < 0)
-        goto error;
+    // so that it can begin handling guest requests as well. (dom0 only)
+    int request_sem_eventfd = -1;
+    if (is_dom0) {
+        request_sem_eventfd = eventfd_sem_init(1);
+        if (request_sem_eventfd < 0)
+            goto error;
 
-    struct ipc_message ipc_msg = {
-        .type = IPC_TYPE_CMD,
-        .cmd = {
-            .command = IVSHMEM_IPC_CMD_START_LISTENING,
-        },
-        .fd_count = 1,
-        .fds = {request_sem_eventfd},
-        .dest = IPC_DEST_IVSHMEM,
-        .flags = IPC_FLAG_WANTRESP | IPC_FLAG_FD,
-    };
+        struct ipc_message ipc_msg = {
+            .type = IPC_TYPE_CMD,
+            .cmd = {
+                .command = IVSHMEM_IPC_CMD_START_LISTENING,
+            },
+            .fd_count = 1,
+            .fds = {request_sem_eventfd},
+            .dest = IPC_DEST_IVSHMEM,
+            .flags = IPC_FLAG_WANTRESP | IPC_FLAG_FD,
+        };
 
-    struct ipc_message ipc_resp;
-    if (!ipc_send_message(&ipc_msg, &ipc_resp))
-        goto error;
-    if (ipc_resp.resp.error)
-        goto error;
+        struct ipc_message ipc_resp;
+        if (!ipc_send_message(&ipc_msg, &ipc_resp))
+            goto error;
+        if (ipc_resp.resp.error)
+            goto error;
+    }
 
     // Poll for events
     struct epoll_event events[5];
@@ -673,10 +676,15 @@ void run_localhandler_loop(int mainsoc, bool is_dom0) {
                 if (!client)
                     log_BUG("Failed to find client!");
 
-                // Valid message received, obtain client request semaphore and handle it
-                eventfd_sem_wait(request_sem_eventfd);
-                bool res = handle_client_message(client, &msg);
-                eventfd_sem_post(request_sem_eventfd);
+                // Valid message received, obtain client request semaphore (dom0 only) and handle it
+                bool res;
+                if (is_dom0) {
+                    eventfd_sem_wait(request_sem_eventfd);
+                    res = handle_client_message(client, &msg);
+                    eventfd_sem_post(request_sem_eventfd);
+                } else {
+                    res = handle_client_message(client, &msg);
+                }
 
                 if (!res)
                     if (!remove_connection(&data, fd))